1aef5703b4b19ceb294eeb530ad0d83a36568050
ashfinenayet <ashfin.enayet@gmail.com>
2020-06-17 11:01:13 -0400

    correcting image

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,10 +19,13 @@ function Index() {
       </Helmet>
       <div id="shadowBox">
       <h1 className="rainbow rainbow_text_animated">swag</h1>
+      </div>
+      <div className="image">
       <img src="https://i.imgur.com/sjvtlq0.png" alt="" />
+      </div>
       <h2>The date according to Node.js (TypeScript) is:</h2>
       <p>{date ? date : 'Loading date...'}</p>
-      </div>
+      
     </main>
   );
 }
