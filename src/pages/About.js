c92d9028c266e642a6cdb0b344f5053e4d00bae2
ashfinenayet <ashfin.enayet@gmail.com>
2020-06-25 14:43:14 -0400

    updated css style sheets for the about page as a test it works

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import '../styles/About.css';
 
- const About = () => (
-    <div>
-        <h1>About Me</h1>
-        <h2>Ashfin Enayet</h2>
-        <p>that is quite literally my name. Currently an incoming freshman at Virginia Commonwealth University.</p>
-        
-    </div>
+const About = () => (
+    <main>
+        <div>
+            <h1>About Me</h1>
+            <h2>Ashfin Enayet</h2>
+            <p>that is quite literally my name. Currently an incoming freshman at Virginia Commonwealth University.</p>
+
+        </div>
+    </main>
 )
 
 export default About;
