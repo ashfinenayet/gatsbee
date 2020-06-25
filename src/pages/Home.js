59278c0074b1bb9e4d8fb9b376e284d3a387205c
ashfinenayet <ashfin.enayet@gmail.com>
2020-06-25 15:58:08 -0400

    GO

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Home = () => (
   <div>
     <h1>Contact Me</h1>
-    <h2>My email</h2>
+    <h2>My email: </h2>
     <p>that is quite literally my email</p>
 
   </div>
