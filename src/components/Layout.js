ac905663b54372d095a9cf8c4c3b25e8f8921900
ashfinenayet <ashfin.enayet@gmail.com>
2020-06-26 00:13:06 -0400

    changed colors centered some stuff yadda yadda

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,9 +10,9 @@ export default function Layout({ children }) {
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
       <header style={{ marginBottom: `1.5rem` }}>
         <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-          <h3 style={{ display: `inline` }}>Ashfin Enayet</h3>
+          <h3 style={{ color: '#D83F87', display: `inline` }}>Ashfin Enayet</h3>
         </Link>
-        <ul style={{ listStyle: `none`, float: `right` }}>
+        <ul style={{ color: '#66FCF1', listStyle: `none`, float: `right` }}>
           <ListLink to="/">Home</ListLink>
           <ListLink to="/About/">About</ListLink>
           <ListLink to="/contact/">Contact</ListLink>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/index.css';
 import Layout from '../components/Layout.js';
 
