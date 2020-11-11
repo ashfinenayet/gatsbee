c3483e5d888688af0c37e1785436f34c573c5110
ashfinenayet <ashfin.enayet@gmail.com>
2020-11-11 13:41:38 -0500

    fixed it

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { graphql, Link } from "gatsby";
 import "./blogTemplate.css";
-import Layout from "../components/Layout";
+
 
 export default function Template({ data }) {
     const post = data.markdownRemark;
     const { title, author, date } = post.frontmatter;
     
     return (
-      <Layout>
+      
         <Link to="/Blog/">Back to blogs</Link>
         <div className="blog">
         <h1>{title}</h1>
         <p>Posted by {author} on {date}</p>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
         </div>
-      </Layout>
+     
     )
   };
 
